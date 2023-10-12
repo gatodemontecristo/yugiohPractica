db76d38d1a7467db72dcaf771eb326854790e0d4
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-10-12 03:40:42 -0500

    Finalizado el componente Carta

--- src/componentes/Carta.jsx
+++ src/componentes/Carta.jsx
@@ -1,4 +1,3 @@
-import  "../estilos/carta-estilo.css";    
 import {
   Card,
   CardBody,
@@ -10,8 +9,8 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Fragment } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShieldHalved } from "@fortawesome/free-solid-svg-icons";
-
+import { faShieldHalved, faHandFist } from "@fortawesome/free-solid-svg-icons";
+import "../estilos/carta-estilo.css";
 
 export const Carta = () => {
   return (
@@ -21,34 +20,57 @@ export const Carta = () => {
           width: "18rem",
         }}
       >
-        <img alt="Sample" src="https://picsum.photos/300/200" />
+        <div className="custom-titulo">
+          <CardTitle tag="h5" style={{marginBottom:"0px"}} >Tornado Dragon</CardTitle>
+          <div style={{display:"flex", alignItems:"center"}}>
+            <p>4</p>
+            <img className="" src="./otros/level.png" alt="" />
+          </div>
+        </div>
+
+        <div className="custom-imagenes">
+          <img
+            className="custom-imagenes-01"
+            alt="Sample"
+            src="https://images.ygoprodeck.com/images/cards_cropped/6983839.jpg"
+          />
+          <img
+            className="custom-imagenes-02"
+            src="./atributos/LAUGH.svg"
+            alt=""
+          />
+          <div className="custom-ataque">
+            <p>
+              <FontAwesomeIcon icon={faHandFist} />
+              Ataque
+            </p>
+          </div>
+          <div className="custom-defensa">
+            <p>
+              <FontAwesomeIcon icon={faShieldHalved} />
+              Defensa
+            </p>
+          </div>
+        </div>
         <CardBody>
-        <div style={{display:"flex"}}>
-          <CardSubtitle className="m-2 p-2 text-muted " tag="h6">
-            Wyrm
-          </CardSubtitle>
-          <CardSubtitle className="m-2 p-2 text-muted" tag="h6">
-            Xyz
-          </CardSubtitle>
-          <CardSubtitle className="m-2 p-2 text-muted" tag="h6">
-            Effect
-          </CardSubtitle>
-
-         </div>
-
-          <img src="./atributos/LAUGH.svg" alt="" />
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardTitle tag="h5">Card title</CardTitle>
-
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card‘s content.
-          </CardText>
-          <Button>
-            {" "}
-            <FontAwesomeIcon icon={faShieldHalved} />
-            Button
-          </Button>
+          <div style={{ display: "flex" }}>
+            <CardSubtitle className="custom-subtitle m-1 p-2" tag="h6">
+              Wyrm
+            </CardSubtitle>
+            <CardSubtitle className="custom-subtitle m-1 p-2" tag="h6">
+              Xyz
+            </CardSubtitle>
+            <CardSubtitle className="custom-subtitle m-1 p-2" tag="h6">
+              Effect
+            </CardSubtitle>
+          </div>
+          <div style={{marginTop:"15px"}}>
+            <p>2 Level 4 monsters</p>
+            <p>
+              Once per turn (Quick Effect): You can detach 1 material from this
+              card, then target 1 Spell/Trap on the field; destroy it.
+            </p>
+          </div>
         </CardBody>
       </Card>
     </Fragment>
