46cf85d4258c196afe72b441da7f1210844453aa
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-10-11 23:26:50 -0500

    Estilos de carta

--- src/componentes/Carta.jsx
+++ src/componentes/Carta.jsx
@@ -1,8 +1,17 @@
-import { Card,CardBody,CardTitle,CardSubtitle,CardText,Button } from "reactstrap";
+import  "../estilos/carta-estilo.css";    
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Fragment } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShieldHalved } from "@fortawesome/free-solid-svg-icons";
+
 
 export const Carta = () => {
   return (
@@ -14,22 +23,34 @@ export const Carta = () => {
       >
         <img alt="Sample" src="https://picsum.photos/300/200" />
         <CardBody>
-          <CardTitle tag="h5">
-            Card title
-            </CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6">
-            Card subtitle  
-            <img src="./atributos/LAUGH.svg" alt="" />
+        <div style={{display:"flex"}}>
+          <CardSubtitle className="m-2 p-2 text-muted " tag="h6">
+            Wyrm
           </CardSubtitle>
+          <CardSubtitle className="m-2 p-2 text-muted" tag="h6">
+            Xyz
+          </CardSubtitle>
+          <CardSubtitle className="m-2 p-2 text-muted" tag="h6">
+            Effect
+          </CardSubtitle>
+
+         </div>
+
+          <img src="./atributos/LAUGH.svg" alt="" />
+          <CardTitle tag="h5">Card title</CardTitle>
+          <CardTitle tag="h5">Card title</CardTitle>
+
           <CardText>
             Some quick example text to build on the card title and make up the
-            bulk of the card‘s content. 
+            bulk of the card‘s content.
           </CardText>
-          <Button> <FontAwesomeIcon icon={faShieldHalved} />Button</Button>
+          <Button>
+            {" "}
+            <FontAwesomeIcon icon={faShieldHalved} />
+            Button
+          </Button>
         </CardBody>
       </Card>
-     
     </Fragment>
-    
   );
 };
