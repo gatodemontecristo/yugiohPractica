f2035931351bedd3fcde38516689e7b1e5853bf5
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-03-23 19:36:50 -0500

    Agregado todos los tipos de mountros xyz, ritual, pendulo, link, fusion

--- src/componentes/BuscarEstilo.jsx
+++ src/componentes/BuscarEstilo.jsx
@@ -2,7 +2,17 @@
 
 export const BuscarEstilo = (tipo) => {
     let estilo;
-    if(tipo.includes("Monster")){
+    if(tipo.includes("XYZ")){
+      estilo = "custom-cuerpo-mounstroXYZ";
+    }else if(tipo.includes("Fusion")){
+      estilo = "custom-cuerpo-mounstroFusion";
+    }else if(tipo.includes("Pendulum")){
+      estilo = "custom-cuerpo-mounstroPendulo";
+    }else if(tipo.includes("Link")){
+      estilo = "custom-cuerpo-mounstroLink";
+    }else if(tipo.includes("Synchro")){
+      estilo = "custom-cuerpo-mounstroSynchro";
+    }else if(tipo.includes("Monster")){
       estilo = "custom-cuerpo-mounstro";
     }else if(tipo.includes("Trap")){
       estilo = "custom-cuerpo-trampa";
