027a0586164bc8006500f510b626cd6a3d794a45
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2024-03-24 00:29:54 -0500

    Agregado el mensaje not found

--- src/componentes/CardGrid.jsx
+++ src/componentes/CardGrid.jsx
@@ -2,24 +2,28 @@ import { useFetchCards } from "../hooks/useFetchCards";
 import { Carta } from "./Carta";
 import "../estilos/carta-tabla.css";
 
-export const CardGrid = ({nombre}) => {
-    const {cartas,isLoading} = useFetchCards(nombre); 
+export const CardGrid = ({ nombre }) => {
+  const { cartas, isLoading } = useFetchCards(nombre);
 
-    
-    return (
-        <>
-          <h3 className="coleccion-titulo">Palabra buscada : "{nombre}"</h3>
-          {
-            isLoading && (
-              <span class="loader"></span>
-            )
-          }
-          <div className="coleccion-cartas">
-            {cartas.map((propiedades) => (
+  return (
+    <>
+      <h3 className="coleccion-titulo">Palabra buscada : "{nombre}"</h3>
+      {isLoading ? (
+        <span class="loader"></span>
+      ) : (
+        <div className="coleccion-cartas">
+          {cartas.length > 0 ? (
+            cartas.map((propiedades) => (
               <Carta key={propiedades.id} {...propiedades}></Carta>
-            ))}
-          </div>
-        </>
-      );
-}
-
+            ))
+          ) : (
+            <div className="coleccion-mensaje">
+              <img src="./otros/kaibanotfound.jpg" alt="" />
+              <h3>No se encontraron cartas :(</h3>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
